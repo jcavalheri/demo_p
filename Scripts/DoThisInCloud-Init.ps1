875085eee1837fa2ed75ec73622e5e5eabb52a92
jcavalheri <zecavalheri@gmail.com>
2022-07-14 10:33:14 +0200

    demo things

---
+++ Scripts/DoThisInCloud-Init.ps1
@@ -0,0 +1,13 @@
+# Download to install folder
+cd C:\install
+
+# Download Firewall Rules
+# Source URL
+$url = "https://raw.githubusercontent.com/jcavalheri/demo_p/main/Scripts/downloadScripts.ps1"
+# Destation file
+$dest = "c:\install\downloadScripts.ps1"
+# Download the file
+(New-Object System.Net.WebClient).DownloadFile($url, $dest)
+
+# Start installation of scripts
+ "./c:\install\downloadScripts.ps1"
