ffe79e5106fcf55289099eb258c84a34f59da44d
jcavalheri <zecavalheri@gmail.com>
2022-07-14 11:34:12 +0200

    change chrome

--- Scripts/install_chome.ps1
+++ Scripts/install_chome.ps1
@@ -1,8 +1,3 @@
-#$LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object    System.Net.WebClient).DownloadFile('https://github.com/jcavalheri/demo_p/tree/main/Executables/ChromeSetup.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
-
-
-
-
 # This script will download the file and do installation
 
  # Download to install folder
